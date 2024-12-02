b47ce8758d5f14b90b06b55602538925d7ef4ec6
Sharmin Shanta <mimetyshanta@gmail.com>
2024-12-02 23:23:18 +0600

    #1: String Concatenation

---
+++ mad-libs.py
@@ -0,0 +1,10 @@
+
+# Issue Reference: https://github.com/sharminshanta/beginner-python-project/issues/1
+# String Concatenation
+
+adj = input("Adjective: ")
+verb = input("Verb: ")
+
+madlib = f'Programming is {adj}, if you are able to {verb}.'
+
+print(madlib)
